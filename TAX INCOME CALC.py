32f0100c4b3d7d6b0222251917946f768a786432
Madk7 <128270656+Madk7@users.noreply.github.com>
2023-03-18 23:44:22 -0400

    Update TAX INCOME CALC.py

--- TAX INCOME CALC.py
+++ TAX INCOME CALC.py
@@ -2,10 +2,10 @@
 
 #Given info:
 #Flat tax rate of 20% (convert to decimal)
-#$10,000 standard deduction (minus?)
+#$10,000 standard deduction (minus)
 #Dependents = $3,000 deduction (multiply number then subtract)
-#gross income to 2 decimal point
-#income tax = decimal number
+#Gross income to 2 decimal point
+#Income tax = decimal number
 
 #Start with customer request phase:
 #gross income, number of dependents
